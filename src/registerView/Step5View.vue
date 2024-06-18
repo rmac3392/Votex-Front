9323e3514d5ba7400aeb34ad402a16b8a1b1f3bd
rmac3392 <ryanjamesmacawili@gmail.com>
2024-06-18 18:07:26 +0800

    Id Card

--- src/registerView/Step5View.vue
+++ src/registerView/Step5View.vue
@@ -3,65 +3,78 @@
     <div class="h-[10%] text-3xl font-semibold text-primary">
       Identification Card
     </div>
-    <div class="flex items-center justify-center h-[90%]">
-      <div class="w-1/2 h-full flex items-center justify-center">
-        <div
-          class="flex items-end bg-white rounded-xl h-64 w-[28rem] shadow-[rgba(0,_0,_0,_0.35)_0px_2px_15px]"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="rounded-b-xl"
-            viewBox="0 0 1440 320"
+    <div class="flex items-center justify-center  h-[90%]">
+      <div class="w-1/2 h-full ">
+        
+        <div class="flex h-[90%] items-center   justify-center">
+          <div
+            class="bg-white z-10 flex items-end rounded-xl h-64 w-[28rem] shadow-[rgba(0,_0,_0,_0.35)_0px_2px_15px]"
           >
-            <defs>
-              <linearGradient
-                id="customGradient"
-                gradientTransform="rotate(90)"
-              >
-                <stop offset="20%" stop-color="rgba(175, 179, 198, 1)" />
-                <stop offset="100%" stop-color="rgba(85, 82, 150, 1)" />
-              </linearGradient>
-            </defs>
-            <path
-              fill="url(#customGradient)"
-              fill-opacity="1"
-              d="M0,0L80,48C160,96,320,192,480,202.7C640,213,800,139,960,101.3C1120,64,1280,64,1360,64L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
-            ></path>
-          </svg>
-        </div>
-      </div>
-      <div class="w-1/2 h-full flex items-center justify-center">
-        <div
-          class="bg-white flex items-end rounded-xl h-64 w-[28rem] shadow-[rgba(0,_0,_0,_0.35)_0px_2px_15px]"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="rounded-b-xl"
-            viewBox="0 0 1440 320"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="rounded-b-xl"
+              viewBox="0 0 1440 320"
+            >
+              <defs>
+                <linearGradient
+                  id="customGradient"
+                  gradientTransform="rotate(90)"
+                >
+                  <stop offset="20%" stop-color="rgba(175, 179, 198, 1)" />
+                  <stop offset="100%" stop-color="rgba(85, 82, 150, 1)" />
+                </linearGradient>
+              </defs>
+              <path
+                fill="url(#customGradient)"
+                fill-opacity="1"
+                d="M0,0L80,48C160,96,320,192,480,202.7C640,213,800,139,960,101.3C1120,64,1280,64,1360,64L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+              ></path>
+            </svg>
+          </div>
+          <div class="bg-red-400 absolute z-10 items-end rounded-xl h-64 w-[28rem]">
             
-          >
-            <defs>
-              <linearGradient
-                id="customGradient"
-                gradientTransform="rotate(90)"
-              >
-                <stop offset="20%" stop-color="rgba(175, 179, 198, 1)" />
-                <stop offset="100%" stop-color="rgba(85, 82, 150, 1)" />
-              </linearGradient>
-            </defs>
-            <path
-              fill="url(#customGradient)"
-              fill-opacity="1"
-              d="M0,0L80,48C160,96,320,192,480,202.7C640,213,800,139,960,101.3C1120,64,1280,64,1360,64L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
-            ></path>
-          </svg>
+          </div>
         </div>
+        <div class="flex items-start justify-center  font-medium text-2xl h-[10%] ">Front</div>
+       
+      </div>
+      <div class="w-1/2 h-full ">
+        <div class="flex h-[90%] items-center justify-center">
+          <div
+            class="bg-white z-10 flex items-end rounded-xl h-64 w-[28rem] shadow-[rgba(0,_0,_0,_0.35)_0px_2px_15px]"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="rounded-b-xl"
+              viewBox="0 0 1440 320"
+            >
+              <defs>
+                <linearGradient
+                  id="customGradient"
+                  gradientTransform="rotate(90)"
+                >
+                  <stop offset="20%" stop-color="rgba(175, 179, 198, 1)" />
+                  <stop offset="100%" stop-color="rgba(85, 82, 150, 1)" />
+                </linearGradient>
+              </defs>
+              <path
+                fill="url(#customGradient)"
+                fill-opacity="1"
+                d="M0,0L80,48C160,96,320,192,480,202.7C640,213,800,139,960,101.3C1120,64,1280,64,1360,64L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+              ></path>
+            </svg>
+          </div>
+          <div class="bg-red-400 absolute z-10 items-end rounded-xl h-64 w-[28rem]">
+            
+          </div>
+        </div>
+        <div class="flex items-start justify-center  font-medium text-2xl h-[10%] ">Back</div>
+        <!-- <div class="bg-red-400 absolute z-10 items-end rounded-xl h-64 w-[28rem]"></div> -->
       </div>
     </div>
   </div>
 </template>
 <script setup></script>
-
 <!-- 
   box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
  -->
